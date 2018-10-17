bf810fe56184d65ea346f9c200ae208db3af957a
richard12511 <richardnew12511@gmail.com>
2018-10-17 09:08:26 -0500

    initial commit

---
+++ lambda_test.py
@@ -0,0 +1,12 @@
+import os
+
+def lambda_handler(event, context):
+    what_to_print = os.environ.get("what_to_print")
+    how_many_times = int(os.environ.get("how_many_times"))
+
+    #make sure what to print and how_many_times values exist
+    if what_to_print and how_many_times > 0:
+        for i in range(0, how_many_times):
+            print(f"what_to_print: {what_to_print}.")
+        return what_to_print
+    return None
